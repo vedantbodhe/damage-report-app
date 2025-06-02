04b91c8b0f8120a1a8c8e0bf2987f29795e767ec
fdlt3859 <vedant-vinayak.bodhe@lt.hs-fulda.de>
2025-06-02 15:01:19 +0200

    Use platform hook (prebuild) to install Tesseract & ZBar on AL2023 again

--- .platform/hooks/prebuild/01_install_system_packages.sh
+++ .platform/hooks/prebuild/01_install_system_packages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-dnf install -y tesseract tesseract-langpack-deu zbar zbar-devel \
+dnf install -y tesseract tesseract-langpack-eng tesseract-langpack-deu zbar zbar-devel \
   libjpeg-turbo-devel libpng-devel freetype-devel libtiff-devel libwebp-devel
 
 ln -sf /usr/bin/tesseract /usr/local/bin/tesseract || true
