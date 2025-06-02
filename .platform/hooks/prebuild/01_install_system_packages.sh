37db0710dfa74d7e55bb88ebc90e0feac3a3ed1a
fdlt3859 <vedant-vinayak.bodhe@lt.hs-fulda.de>
2025-06-02 15:37:04 +0200

    install tesseract + zbar via platform hook

--- .platform/hooks/prebuild/01_install_system_packages.sh
+++ .platform/hooks/prebuild/01_install_system_packages.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-dnf install -y tesseract tesseract-langpack-eng tesseract-langpack-deu zbar zbar-devel \
-  libjpeg-turbo-devel libpng-devel freetype-devel libtiff-devel libwebp-devel
+dnf -y install \
+    tesseract \
+    tesseract-langpack-deu \
+    zbar \
+    zbar-devel \
+    libjpeg-turbo-devel \
+    libpng-devel \
+    freetype-devel \
+    libtiff-devel \
+    libwebp-devel
 
 ln -sf /usr/bin/tesseract /usr/local/bin/tesseract || true
